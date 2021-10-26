4bac249402a1d05e91230e9c823a6f8bd4ef04d9
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-10-26 13:30:37 -0500

    added state and componentDidMount to App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from 'react'
+import axios from 'axios'
 import User from './components/User'
 import './App.css'
 
 class App extends React.Component {
+  state = {
+    data: {},
+    input: 'mrjacobsullivan',
+  }
+
+  componentDidMount() {
+    axios
+      .get(`https://api.github.com/users/${this.state.input}`)
+      .then((res) => {
+        this.setState({ ...this.state, data: res.data })
+      })
+      .catch((err) => console.error(err))
+  }
+
   render() {
     return (
       <main>
         <h1>Github Info</h1>
 
         <form>
-          <input type='text' />
+          <input type='text' value={this.state.input} />
           <button>Search</button>
         </form>
 
