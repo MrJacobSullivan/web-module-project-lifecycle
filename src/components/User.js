8179370b0902338e27a0e719d34b7d04e63603ad
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-10-26 13:21:42 -0500

    added DOM structure to User

--- src/components/User.js
+++ src/components/User.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import FollowerList from './FollowerList'
 
 class User extends React.Component {
   render() {
-    return null
+    return (
+      <section>
+        <img src='' alt='' />
+        <h2>@username</h2>
+
+        <p>Total Repos: 0</p>
+        <p>Total Followers: 0</p>
+
+        <FollowerList />
+      </section>
+    )
   }
 }
 
