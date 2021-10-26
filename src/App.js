45ea97a18e2c558c4e60e15b91752d2d8656fd14
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-10-26 13:13:38 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React from 'react';
+import React from 'react'
 
 class App extends React.Component {
   render() {
-    return(<div>
-      Github Card
-    </div>);
+    return <div>Github Card</div>
   }
 }
 
-export default App;
+export default App
